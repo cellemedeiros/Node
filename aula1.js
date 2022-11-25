65e5dc293766c45d82b0ac7b4c07c9b0567c3544
Marcelle Medeiros <112626750+cellemedeiros@users.noreply.github.com>
2022-11-25 10:29:12 -0300

    Add files via upload

--- aula1.js
+++ aula1.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function(req, res) {
-    res.write('Brasil 3x0 Servia \n');
+    res.write('Brasil 2x0 Servia')
     res.end('Bom dia!');
 
 }).listen(3000);
